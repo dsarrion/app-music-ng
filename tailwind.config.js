5aa911183bae9e315c827e8b2c6e8484e499a94e
dsarrion <samidas@msn.com>
2024-04-03 13:37:18 +0200

    n diseño

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     "./node_modules/flowbite/**/*.js"
   ],
   theme: {
-    extend: {},
+    extend: {
+      dropShadow: {
+        '3xl': '0 45px 65px rgba(255, 255, 255, 0.8)'   
+      }
+    },
   },
   plugins: [
     require('flowbite/plugin')
